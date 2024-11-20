c4c1c6930701211a70bc46d52ef99e2b8e086739
tmatkovic <toni.matkovic0@gmail.com>
2024-11-20 18:22:54 +0000

    Refacored parametrized emits

--- app/src/components/NewRestaurantForm.vue
+++ app/src/components/NewRestaurantForm.vue
@@ -1,21 +1,33 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import { reactive } from 'vue'
 import { v4 as uuidv4 } from 'uuid'
 import { restaurantStatusList } from '@/constants'
+import { Restaurant } from '@/types'
 
-export default defineComponent({
-  emits: ['add-new-restaurant', 'cancel-new-restaurant'],
-  data: () => ({
-    newRestaurant: {
-      id: uuidv4(),
-      name: '',
-      address: '',
-      website: '',
-      status: 'Want to Try',
-    },
-    restaurantStatusList,
-  }),
+type State = {
+  newRestaurant: Restaurant
+}
+
+const emits = defineEmits<{
+  (e: 'add-new-restaurant', restaurant: Restaurant): void
+  (e: 'cancel-new-restaurant'): void
+}>()
+const state = reactive<State>({
+  newRestaurant: {
+    id: uuidv4(),
+    name: '',
+    address: '',
+    website: '',
+    status: 'Want to Try',
+  },
 })
+
+const addRestaurant = () => {
+  emits('add-new-restaurant', state.newRestaurant)
+}
+const cancelRestaurant = () => {
+  emits('cancel-new-restaurant')
+}
 </script>
 
 <template>
@@ -25,7 +37,7 @@ export default defineComponent({
         <label for="name" class="label">Name</label>
         <div class="control">
           <input
-            :value="newRestaurant.name"
+            :value="state.newRestaurant.name"
             @keyup.space="updateName"
             type="text"
             class="input is-large"
@@ -38,13 +50,18 @@ export default defineComponent({
       <div class="field">
         <label for="website" class="label">Website</label>
         <div class="control">
-          <input v-model="newRestaurant.website" type="text" class="input" placeholder="www.beignetandthejets.com" />
+          <input
+            v-model="state.newRestaurant.website"
+            type="text"
+            class="input"
+            placeholder="www.beignetandthejets.com"
+          />
         </div>
       </div>
       <div class="field mb-5">
         <label for="status" class="label">Status</label>
         <div class="select">
-          <select v-model="newRestaurant.status" id="status">
+          <select v-model="state.newRestaurant.status" id="status">
             <option v-for="status in restaurantStatusList" :value="status" :key="`option-${status}`">
               {{ status }}
             </option>
@@ -53,8 +70,8 @@ export default defineComponent({
       </div>
       <div class="field">
         <div class="buttons">
-          <button @click="$emit('add-new-restaurant', newRestaurant)" class="button is-success">Create</button>
-          <button @click="$emit('cancel-new-restaurant')" class="button is-light">Cancel</button>
+          <button @click="addRestaurant" class="button is-success">Create</button>
+          <button @click="cancelRestaurant" class="button is-light">Cancel</button>
         </div>
       </div>
     </div>
