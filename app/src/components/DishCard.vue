71b92703e8693720dd334fd0c3548104ebabb142
tmatkovic <toni.matkovic0@gmail.com>
2024-11-20 15:45:44 +0000

    Typescript props & emits in DishCard

--- app/src/components/DishCard.vue
+++ app/src/components/DishCard.vue
@@ -1,36 +1,29 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import { computed, defineComponent } from 'vue'
 import type { PropType } from 'vue'
 import type { Dish } from '@/types'
 
-export default defineComponent({
-  props: {
-    dish: {
-      type: Object as PropType<Dish>,
-      required: true,
-    },
-  },
-  emits: ['delete-dish'],
-  computed: {
-    statusColor() {
-      switch (this.dish.status) {
-        case 'Want to Try':
-          return 'is-warning'
-        case 'Recommended':
-          return 'is-success'
-        case 'Do Not Recommend':
-          return 'is-danger'
-        default:
-          return ''
-      }
-    },
-  },
-  methods: {
-    deleteDish() {
-      this.$emit('delete-dish', this.dish)
-    },
-  },
+type Props = {
+  dish: Dish
+}
+
+const props = defineProps<Props>()
+const emits = defineEmits<{ (e: 'delete-dish', dish: Dish): void }>()
+const statusColor = computed(() => {
+  switch (props.dish.status) {
+    case 'Want to Try':
+      return 'is-warning'
+    case 'Recommended':
+      return 'is-success'
+    case 'Do Not Recommend':
+      return 'is-danger'
+    default:
+      return ''
+  }
 })
+const deleteDish = () => {
+  emits('delete-dish', props.dish)
+}
 </script>
 
 <template>
